7dbbbdcf6b6e98c0431aecb385718ac25f2b78be
Edgar eduardo <eepsmr48@hotmail.com>
2021-08-18 14:10:48 -0500

    Add project files.

---
+++ cam/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace cam
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ cam/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cam
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private bool HayDispositivos;
+        private FilterInfoCollection MiDispositivos;
+        private VideoCaptureDevice MiWebCam;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            CargaDispositivos();
+        }
+        public void CargaDispositivos()
+        {
+            MiDispositivos = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (MiDispositivos.Count > 0)
+            {
+                HayDispositivos = true;
+                for (int i = 0; i < MiDispositivos.Count; i++)
+                    comboBox1.Items.Add(MiDispositivos[i].Name.ToString());
+                comboBox1.Text = MiDispositivos[0].ToString();
+            }
+            else
+            {
+                HayDispositivos = false;
+            }
+        }
+        public void CerrarWebCam()
+        {
+            if (MiWebCam != null && MiWebCam.IsRunning)
+            {
+                MiWebCam.SignalToStop();
+                MiWebCam = null;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CerrarWebCam();
+            int i = comboBox1.SelectedIndex;
+            string NombreVideo = MiDispositivos[i].MonikerString;
+            MiWebCam = new VideoCaptureDevice(NombreVideo);
+            MiWebCam.NewFrame += new NewFrameEventHandler(Capturando);
+            MiWebCam.Start();
+        }
+        private void Capturando(object sender, NewFrameEventArgs eventArgs)
+        {
+            System.Drawing.Image imgforms = (Bitmap)eventArgs.Frame.Clone();
+
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+
+            MemoryStream ms = new MemoryStream();
+            imgforms.Save(ms, ImageFormat.Bmp);
+            ms.Seek(0, SeekOrigin.Begin);
+
+            bi.StreamSource = ms;
+            bi.EndInit();
+
+            bi.Freeze();
+            Dispatcher.BeginInvoke(new ThreadStart(delegate
+            {
+                VideoContainer2.Source = bi;
+            }));
+        }
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            CerrarWebCam();
+        }
+    }
+}
